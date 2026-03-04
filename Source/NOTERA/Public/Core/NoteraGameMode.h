b3b99540d84a0adba502d8de64419bd1e145fa4e
бЛЯАТ <36612185+q-0z@users.noreply.github.com>
2026-03-04 09:25:28 +0530

    Updated CPP Gamemode

---
+++ Source/NOTERA/Public/Core/NoteraGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "NoteraGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NOTERA_API ANoteraGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
